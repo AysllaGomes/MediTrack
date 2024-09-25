0e5d240c3b38f5cc7140fde25ce19a9f187764c7
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-09-25 08:33:11 -0300

    [ADD] [TASK-????] Tipando o retorno de user

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -6,23 +6,24 @@ import { CreateUserDto } from '../models/create-user.dto';
 import { UpdateUserDto } from '../models/update-user.dto';
 
 import { ValidateObjectIdPipe } from '../../shared/pipes/ValidateObjectId.pipe';
+import { User } from '../../schemas/user.schema';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<User[]> {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  findById(@Param('id', ValidateObjectIdPipe) id: string) {
+  findById(@Param('id', ValidateObjectIdPipe) id: string): Promise<User> {
     return this.userService.findById(id);
   }
 
@@ -30,7 +31,7 @@ export class UserController {
   update(
     @Param('id', ValidateObjectIdPipe) id: string,
     @Body() updateUserDto: UpdateUserDto
-  ) {
+  ): Promise<User> {
     return this.userService.update(id, updateUserDto);
   }
 }
